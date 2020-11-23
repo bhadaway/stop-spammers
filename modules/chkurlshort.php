9b384e97e81cba9f8b2fd9fe4e7e12555c98b9c2
Hitesh Chandwani <hiteshchandwani98@gmail.com>
2020-11-23 21:25:28 +0530

    Shorturl Fix
    Shorturl Fix for domain containing part of shorurl.

--- modules/chkurlshort.php
+++ modules/chkurlshort.php
@@ -4,17 +4,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class chkurlshort {
-// look on option list for spam words
-	public function process(
-		$ip, &$stats = array(), &$options = array(), &$post = array()
-	) {
-// Checks for links from common URL shortening services
-// 'email', 'author', 'pwd', 'comment', 'subject'
+	// look on option list for spam words
+	public function process( $ip, &$stats = array(), &$options = array(), &$post = array() ) {
+		// Checks for links from common URL shortening services
+		// 'email', 'author', 'pwd', 'comment', 'subject'
 		$denyurlshortners = $options['denyurlshortners'];
 		foreach ( $post as $key => $data ) {
 			if ( ! empty( $data ) ) {
 				foreach ( $denyurlshortners as $urlshort ) {
-					if ( stripos( $data, $urlshort ) !== false ) {
+					if ( stripos( $data, $urlshort ) !== false and ( stripos( $data, $urlshort ) == 0 or substr( $data, stripos( $data, $urlshort ) - 1, 1 ) == " " or substr( $data, stripos( $data, $urlshort ) - 1, 1 ) == "/" or substr( $data, stripos( $data, $urlshort ) - 1, 1 ) == "@" or substr( $data, stripos( $data, $urlshort ) - 1, 1 ) == "." ) ) {
 						return "URL Shortener: $urlshort in $key";
 					}
 				}
