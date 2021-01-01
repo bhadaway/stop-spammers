bfb4d41455dd5196590130f4c363b512b7173e31
Trumani <ryanjgrieco@gmail.com>
2021-01-01 18:48:36 -0500

    Update ss_webservices_settings.php
    web services documentation cleanup

--- settings/ss_webservices_settings.php
+++ settings/ss_webservices_settings.php
@@ -66,7 +66,11 @@ if ( ! empty( $nonce ) && wp_verify_nonce( $nonce, 'ss_stopspam_update' ) ) {
 		ss_set_options( $options );
 		extract( $options ); // extract again to get the new options
 	}
-	$msg = '<div class="notice notice-success is-dismissible"><p>Options Updated</p></div>';
+	if ( ! is_plugin_active( 'stop-spammers-premium/stop-spammers-premium.php' ) ) {
+		$msg = '<div class="notice notice-success is-dismissible"><p>Options Updated! Need a firewall, themable login, honeypot for Divi / Elementor / CF7 / bbPress? — <strong><a href="https://stopspammers.io/downloads/stop-spammers-premium/" target="_blank">Try Premium</a></strong></p></div>';
+	} else {
+		$msg = '<div class="notice notice-success is-dismissible"><p>Options Updated!</p></div>';
+	}
 }
 $nonce = wp_create_nonce( 'ss_stopspam_update' );
 ?>
@@ -75,11 +79,11 @@ $nonce = wp_create_nonce( 'ss_stopspam_update' );
 	<?php if ( ! empty( $msg ) ) {
 		echo "$msg";
 	} ?>
-    <p>There are many services that can be used to check for spam or protect
-        your website against spammers. Most require
-        a key so that only registered users can use their services. All of the
-        services here can be used by Stop
-        Spammers and all are free.</p>
+<br />
+	<div class="ss_info_box">
+    <p>Below are several services that can be enabled to check for spam or protect
+        your website against spammers. To learn more about each service and find links to create keys, please <a href="https://stopspammers.io/web-services" target="_blank">review our documentation</a>.</p></div>
+<br />
     <form method="post" action="">
         <input type="hidden" name="action" value="update" />
         <input type="hidden" name="ss_stop_spammers_control" value="<?php echo $nonce; ?>" />
@@ -88,10 +92,7 @@ $nonce = wp_create_nonce( 'ss_stopspam_update' );
             <input class"ss_toggle" type="checkbox" id="chkdnsbl" name="chkdnsbl" value="Y" <?php if ( $chkdnsbl == 'Y' ) {
 					echo "checked=\"checked\"";
 } ?> /><span><small></small></span>
-		  <small><span style="font-size:16px!important;">Check Against DNSBL Lists Such as Spamhaus.org </span></small></label><i class="fa fa-question-circle fa-2x tooltip"><span class="tooltiptext">
- Checks the IP on Spamhaus.org. This is primarily used for
-            email spam, but the same bots sending out
-            email spam are probably running comment spam and other exploits.</span></i></div>      
+		  <small><span style="font-size:16px!important;">Check Against DNSBL Lists Such as Spamhaus.org</span></small></label></div>      
         <br />		
 
 			<div class="checkbox switcher">
@@ -99,19 +100,9 @@ $nonce = wp_create_nonce( 'ss_stopspam_update' );
             <input class"ss_toggle" type="checkbox" id="chksfs" name="chksfs" value="Y" <?php if ( $chksfs == 'Y' ) {
 					echo "checked=\"checked\"";
 } ?> /><span><small></small></span>
-		  <small><span style="font-size:16px!important;">Enable Stop Forum Spam Lookups </span></small></label><i class="fa fa-question-circle fa-2x tooltip"><span class="tooltiptext">
-                Check against the Stop Forum Spam database.
-				You do not need an API key.</span></i></div>
+		  <small><span style="font-size:16px!important;">Enable Stop Forum Spam Lookups</span></small></label></div>
 				<br />
-			
-                You can set the minimum settings to allow possible spammers to
-                use your site. You may wish to forgive spammers with few incidents or no recent
-                activity. I would recommend that to be
-                on the safe side, you should block users who appear on the spam
-                database unless they specifically ask to
-                be Allow Listed.
-                Allowed values are 0 to 9999. Only numbers are accepted.<br />
-            <table align="center" cellspacing="1" style="background-color:#ccc;font-size:0.9em">
+            <table cellspacing="1" style="background-color:#ccc;font-size:0.9em;margin-left:30px;">
                 <tr bgcolor="white">
                     <td valign="top">Deny spammers found on Stop Forum Spam with
                         more than
@@ -122,34 +113,15 @@ $nonce = wp_create_nonce( 'ss_stopspam_update' );
                     </td>
                 </tr>
             </table>
-
+			<br />
         <br />
-		<fieldset>
-            <legend>
-				<span style="font-weight:bold;font-size:1.2em">StopForumSpam.com API Key</span>
-            </legend>
-			If you want to report any
-                spam that you find, you need to provide it here. You can
-                register and get an API key at <a
-                        href="https://www.stopforumspam.com/keys"
-                        target="_blank">https://www.stopforumspam.com/keys</a>.<br />
-			<br />
+				<span class="keyhead">StopForumSpam.com API Key</span>
             <input size="32" name="apikey" type="text" value="<?php echo $apikey; ?>" /><br />
-		</fieldset>
 			<br />
-        <fieldset>
-            <legend>
-				<span style="font-weight:bold;font-size:1.2em">Project Honeypot API Key</span>
-            </legend>
+				<span class="keyhead">Project Honeypot API Key</span>
             <input size="32" name="honeyapi" type="text" value="<?php echo $honeyapi; ?>" /><br />
-            <p>This API key is used for querying the Project Honeypot Deny List.
-                It is required if you want to
-                check IP addresses against the Project Honeypot database. You
-                can register and get an API key at <a
-                        href="https://www.projecthoneypot.org/account_login.php"
-                        target="_blank">https://www.projecthoneypot.org/account_login.php</a>.<br />
-                Allowed values are 0 to 9999. Only numbers are accepted.</p>
-            <table align="center" cellspacing="1" style="background-color:#ccc;font-size:0.9em">
+			<br />
+            <table cellspacing="1" style="background-color:#ccc;font-size:0.9em;margin-left:30px;">
                 <tr bgcolor="white">
                     <td valign="top">Deny spammers found on Project HoneyPot
                         with incidents less than
@@ -161,23 +133,12 @@ $nonce = wp_create_nonce( 'ss_stopspam_update' );
                     </td>
                 </tr>
             </table>
-        </fieldset>
         <br />
-        <fieldset>
-            <legend>
-				<span style="font-weight:bold;font-size:1.2em">BotScout API Key</span>
-            </legend>
+	<br />
+			<span class="keyhead">BotScout API Key</span>
             <input size="32" name="botscoutapi" type="text" value="<?php echo $botscoutapi; ?>" /><br />
-            <p>This API key is used for querying the BotScout database. It is
-                required if you want to
-                check IP addresses against the botscout.com database. You can
-                register and get an API key at <a
-                        href="https://botscout.com/getkey.htm" target="_blank">https://botscout.com/getkey.htm</a>.<br />
-                Allowed values are 0 to 9999. Only numbers are accepted.<br />
-                <em>Please note that BotScout is disabled in this release
-                    because of policy changes at
-                    botscout.com.</em></p>
-            <table align="center" cellspacing="1" style="background-color:#ccc;font-size:0.9em">
+		<br />
+            <table cellspacing="1" style="background-color:#ccc;font-size:0.9em;margin-left:30px;">
                 <tr bgcolor="white">
                     <td valign="top">Deny spammers found on BotScout with more
                         than
@@ -186,20 +147,10 @@ $nonce = wp_create_nonce( 'ss_stopspam_update' );
                     </td>
                 </tr>
             </table>
-        </fieldset>
         <br />
-        <fieldset>
-            <legend>
-				<span style="font-weight:bold;font-size:1.2em">Google Safe Browsing API Key</span>
-            </legend>
+	<br />
+			<span class="keyhead">Google Safe Browsing API Key</span>
             <input size="32" name="googleapi" type="text" value="<?php echo $googleapi; ?>" /><br />
-            <p><a href="https://developers.google.com/safe-browsing/key_signup"
-               target="_blank">Sign up for a Google Safe
-                Browsing API Key</a> If this API key is present, URLs found in
-            comments will be checked for phishing or
-            malware sites and if found, will be rejected.</p>
-        </fieldset>
-        <br />
         <br />
         <p class="submit"><input class="button-primary" value="Save Changes" type="submit" /></p>
     </form>
