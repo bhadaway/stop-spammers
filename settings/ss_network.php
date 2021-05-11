9367230a65122045e86e29d0e082669c3b31d11e
Hitesh Chandwani <hiteshchandwani98@gmail.com>
2021-05-11 23:55:47 +0530

    Network fix

--- settings/ss_network.php
+++ settings/ss_network.php
@@ -30,7 +30,7 @@ ss_fix_post_vars();
 	if ( wp_verify_nonce( $nonce, 'ss_stopspam_update' ) ) {
 		if ( array_key_exists( 'action', $_POST ) ) {
 			if ( array_key_exists( 'muswitch', $_POST ) ) {
-				$muswitch = trim( stripslashes( sanitize_key( $_POST['muswitch'] ) ) );
+				$muswitch = trim( stripslashes( sanitize_text_field( $_POST['muswitch'] ) ) );
 			}
 			if ( empty( $muswitch ) ) {
 				$muswitch = 'N';
